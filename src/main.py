ea811a478f612156e7c7acaca0af93f51e0a79ac
Evan <evan33r@gmail.com>
2026-01-20 11:51:01 +0100

    completed random video playback

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from moviepy import VideoFileClip
+import signal; import sys
+import random; import os
+
+videos = ["forrestGump_destiny.mp4", 
+          "goodWillHunting_potential.mp4",
+          "jCole_procrastination.mp4",
+          "kobe_failure.mp4",
+          "kungFuPanda_identity.mp4"]
+
+folder_path = "C:\Evan\engProjects\shortMotivations" 
+path = os.path.join(folder_path, random.choice(videos))
+
+def safe_preview():
+    clip = VideoFileClip(path)
+    
+    try:
+        clip.preview(fps=clip.fps, audio=True)
+    except (OSError, IOError, BrokenPipeError):
+        pass
+    finally:
+        clip.close()
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, lambda s, f: sys.exit(0))
+
+if __name__ == "__main__":
+    safe_preview()
+    
